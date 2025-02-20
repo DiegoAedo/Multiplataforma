513e7cb2b5af5641c8810faa592230b91bd80d12
Diego <diego.aedoq@gmail.com>
2025-02-20 13:48:46 -0300

    Compilacion Android

--- src/main/java/com/example/MainMobile.java
+++ src/main/java/com/example/MainMobile.java
@@ -7,17 +7,15 @@ import javafx.scene.control.Label;
 
 public class MainMobile extends MobileApplication {
 
-    public static final String HOME_VIEW = "HOME_VIEW";
-
     @Override
     public void init() {
-        // Se agrega una vista simple que muestra un Label.
+        // Vista simple con un Label
         addViewFactory(HOME_VIEW, () -> new View(new Label("¡Hola Gluon Mobile!")));
     }
 
     @Override
     public void postInit(Scene scene) {
-        // Aquí puedes aplicar un tema o realizar otras configuraciones globales.
+        // Aplicar tema o configuraciones globales
     }
 
     public static void main(String[] args) {
