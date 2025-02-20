4556de2142fbb8e30976e3593e8e500b72304115
Diego <diego.aedoq@gmail.com>
2025-02-20 14:43:23 -0300

    Compilacion Android

--- src/main/java/com/example/MainMobile.java
+++ src/main/java/com/example/MainMobile.java
@@ -9,18 +9,7 @@ public class MainMobile extends MobileApplication {
 
     @Override
     public void init() {
-        System.out.println("Iniciando MainMobile");
-        // Registrar la vista con la clave HOME_VIEW
-        addViewFactory(HOME_VIEW, () -> new View(new Label("¡Hola Gluon Mobile!")));
+        addViewFactory(HOME_VIEW, () -> new View(new Label("Hello Victor!")));
     }
 
-    @Override
-    public void postInit(Scene scene) {
-        // Después de crear el scene, cambiar a la vista registrada para que se renderice
-        switchView(HOME_VIEW);
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
